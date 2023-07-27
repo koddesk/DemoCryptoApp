b8c148b14c8788ffe6d5eb26a5a030f8f0acee8a
Nikolay Dobychin <koddek@bk.ru>
2023-07-27 13:36:36 +0300

    Start project
    Add Colors

---
+++ DemoCrypto/Extensions/Color + Extensions .swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  DemoCrypto
+//
+//  Created by KODDER on 27.07.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = Theme()
+}
+
+
+struct Theme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
