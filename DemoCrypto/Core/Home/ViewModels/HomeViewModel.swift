c5c795b82db2373700b64c80c181084786fc89e9
Nikolay Dobychin <koddek@bk.ru>
2023-07-28 17:32:18 +0300

    Add a dynamic List to display downloaded coins

---
+++ DemoCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewModel.swift
+//  DemoCrypto
+//
+//  Created by KODDER on 28.07.2023.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [Coin] = []
+    @Published var portfolioCoins: [Coin] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+    
+}
