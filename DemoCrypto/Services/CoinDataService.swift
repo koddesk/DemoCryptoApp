51bf8f722955fcc7d41071ac1d280c9bfbf83745
Nikolay Dobychin <koddek@bk.ru>
2023-07-31 18:22:28 +0300

    Download coins from API

---
+++ DemoCrypto/Services/CoinDataService.swift
@@ -0,0 +1,47 @@
+//
+//  CoinDataService.swift
+//  DemoCrypto
+//
+//  Created by KODDER on 31.07.2023.
+//
+
+import Foundation
+import Combine
+
+struct Constants {
+    static let baseURL = "https://api.coingecko.com"
+}
+
+class CoinDataService {
+    
+    @Published var allCoins: [Coin] = []
+    
+    var coinSubscription: AnyCancellable?
+    
+    init() {
+        getCoins()
+    }
+    
+    private func getCoins() {
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=rub&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
+        
+        coinSubscription = NetworkingManager.download(url: url)
+            .decode(type: [Coin].self, decoder: JSONDecoder())
+//            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
+//                self?.allCoins = returnedCoins
+//                self?.coinSubscription?.cancel()
+//            })
+            .sink(receiveCompletion: { (completion) in
+                switch completion {
+                    case .finished:
+                        break
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                }
+            }, receiveValue: { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
+            })
+    }
+}
